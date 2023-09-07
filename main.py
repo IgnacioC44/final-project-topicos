7d5bac3564dc7efa015273af0d2b581a495b2e0f
Ignacio Ivan Cossio Roncal <2788936@gmail.com>
2023-09-07 15:58:09 -0400

    fixed dotenv functionality

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import openai
 import sys
 from dotenv import load_dotenv, find_dotenv
 
+load_dotenv()
+
 #using openai api key and loading data
-os.environ["OPEN_API_KEY"] = "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"
-_ = load_dotenv(find_dotenv())
-openai.api_key = os.environ['OPEN_API_KEY']
+openai.api_key = os.getenv("OPEN_API_KEY")
 
